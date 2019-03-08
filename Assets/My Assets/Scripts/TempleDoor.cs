4bd7ce6ccb7235cb6c0e85aa79c778e970fd75bb
Scott Little <scott.little@loop.colum.edu>
2019-03-08 13:39:15 -0600

    Add and Animate the Temple Door
    - Created a door for the front of the temple and tinted it to be slightly orange to increase visibility
    - Animated the Temple Door to open when the player gets close enough
    - Added a new script for opening the temple door when the player steps close enough

---
+++ Assets/My Assets/Scripts/TempleDoor.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class TempleDoor : MonoBehaviour
+{
+    private Animator templeDoor_Anim;
+    private BoxCollider templeDoor_Col;
+
+    private void Start()
+    {
+        templeDoor_Anim = GetComponent<Animator>();
+        templeDoor_Col = GetComponent<BoxCollider>();
+    }
+    private void OnTriggerEnter(Collider collision)
+    {
+        if(collision.tag == "Player")
+        {
+            Debug.Log("The Door's Collider sees you.");
+            templeDoor_Anim.SetBool("Opened", true);
+        }
+    }
+}
