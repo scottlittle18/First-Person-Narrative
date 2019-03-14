a9ce5b3786eae98784f99743379b061d876d7d6f
Scott Little <scott.little@loop.colum.edu>
2019-03-14 17:37:22 -0500

    Add Particle Effect to Car and Doors to New Temple
    - Added and animated the doors for the New Temple Prototype
    - Added a Particle effect to the car to simulate smoke coming from the engine

---
+++ Assets/My Assets/Scripts/TempleMainDoors.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TempleMainDoors : MonoBehaviour
+{
+    private Animator leftTempleDoor_Anim, rightTempleDoor_Anim;
+    private Animator[] templeDoor_Anim;
+    private BoxCollider templeDoor_Col;
+
+    private void Start()
+    {
+        templeDoor_Anim = GetComponentsInChildren<Animator>();
+        //rightTempleDoor_Anim = GetComponent<Animator>();
+        //templeDoor_Col = GetComponent<BoxCollider>();
+    }
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (collision.tag == "Player")
+        {
+            Debug.Log("The Door's Collider sees you.");
+            foreach (Animator anim in templeDoor_Anim)
+            {
+                anim.SetBool("Open", true);
+            }
+        }
+    }
+}
