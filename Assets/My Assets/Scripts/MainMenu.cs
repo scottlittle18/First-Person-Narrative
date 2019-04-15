d4515309e765d774d676bdb6f4b0557c0f4de4c7
Scott Little <scott.little@loop.colum.edu>
2019-04-14 19:50:39 -0500

    Add MainMenu Script to TitleMenu scene's Camera
    - Added a Credit Layout that appears when the Credits button is clicked
    - Shrunk Main Menu Panel to be more focused in the center of the screen

---
+++ Assets/My Assets/Scripts/MainMenu.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayButtonClicked()
+    {
+        int currentScene = SceneManager.GetActiveScene().buildIndex;
+
+        SceneManager.LoadScene(currentScene + 1);
+    }
+
+    public void CreditsButtonClicked()
+    {
+        //SceneManager.LoadScene("CreditsScene");
+    }
+
+    public void ExitButtonClicked()
+    {
+        //TODO: Debug Quit
+        Debug.Log("Quit!");
+        Application.Quit();
+    }
+}
