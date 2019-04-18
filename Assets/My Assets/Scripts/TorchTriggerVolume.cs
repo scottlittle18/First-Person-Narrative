6578d41a8860227a78cf148456901a93f27adac8
Scott Little <scott.little@loop.colum.edu>
2019-04-18 18:04:44 -0500

    Start scripting the Torches to get turned on when the player enters the trigger volume

---
+++ Assets/My Assets/Scripts/TorchTriggerVolume.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TorchTriggerVolume : MonoBehaviour
+{
+    private List<ParticleSystem> torchFires;
+
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        foreach (GameObject go in GameObject.FindGameObjectsWithTag("TorchFire"))
+        {
+            torchFires.Add(go.GetComponent<ParticleSystem>());
+        }
+
+        foreach (ParticleSystem pS in torchFires)
+        {
+            pS.Stop();
+            //pS.emission.SetBurst(new ParticleSystem.Burst(0.5f, 100);
+        }
+    }
+}
