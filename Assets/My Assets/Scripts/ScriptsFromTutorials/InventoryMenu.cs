61dff9253077116d9b5b5c91e56d603f158827ed
Scott Little <scott.little@loop.colum.edu>
2019-05-04 17:16:44 -0500

    TextMesh Asset Moved without any problems, Beginning work on the InventoryMenu script

---
+++ Assets/My Assets/Scripts/ScriptsFromTutorials/InventoryMenu.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryMenu : MonoBehaviour
+{
+    private static InventoryMenu instance;
+
+    public static InventoryMenu Instance
+    {
+        get
+        {
+            return instance;
+        }
+        private set { instance = value; }
+    }
+
+    private void Awake()
+    {
+        instance = this;
+    }
+}
